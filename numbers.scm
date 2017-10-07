691f7c1aa1c75b1da1bdefabba04c90e48bd6f12
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-07 22:10:33 +0530

    Implement addtup and mul

--- numbers.scm
+++ numbers.scm
@@ -9,3 +9,12 @@
 
 (define (sub a b)
   (if (zero? b) a (sub (sub1 a) (sub1 b))))
+
+
+(define (addtup tup)
+  (if (null? tup)
+      0
+      (add (car tup) (addtup (cdr tup)))))
+
+(define (mul n m)
+  (if (zero? m) 0 (add n (mul n (sub1 m)))))
