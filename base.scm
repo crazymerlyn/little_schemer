3c9a1cfcc657caa086376d91ae3a08eb2621305a
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-07 18:42:42 +0530

    Implement member?

--- base.scm
+++ base.scm
@@ -3,3 +3,7 @@
   (cond ((null? l) #t)
         ((atom? (car l)) (lat? (cdr l)))
         (else #f)))
+(define (member? a lat)
+  (cond ((null? lat) #f)
+        (else (or (eq? a (car lat)))
+              (member? a (cdr lat)))))
