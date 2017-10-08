477d7026b1a2fc8239cf45fad1c5e7d5d1cd2767
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-08 15:02:26 +0530

    Implement intersect?, intersect and union

--- friends_and_relations.scm
+++ friends_and_relations.scm
@@ -19,3 +19,21 @@
 (define (eqset? s1 s2)
   (and (subset? s1 s2)
        (subset? s2 s1)))
+
+(define (intersect? s1 s2)
+  (if (null? s1)
+      #f
+      (or (member? (car s1) s2)
+          (intersect? (cdr s1) s2))))
+
+(define (intersect s1 s2)
+  (cond ((null? s1) '())
+        ((member? (car s1) s2)
+         (cons (car s1) (intersect (cdr s1) s2)))
+        (else (intersect (cdr s1) s2))))
+
+(define (union s1 s2)
+  (cond ((null? s1) s2)
+        ((member? (car s1) s2)
+         (union (cdr s1) s2))
+        (else (cons (car s1) (union (cdr s1) s2)))))
