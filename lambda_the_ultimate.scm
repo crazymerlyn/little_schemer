7f3635c6759d553645afda44ef3d0256eb5f5427
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-08 15:54:57 +0530

    Implement remember-f

---
+++ lambda_the_ultimate.scm
@@ -0,0 +1,6 @@
+(load "base.scm")
+(define (rember-f test?)
+  (lambda (a l)
+    (cond ((null? l) '())
+          ((test? a (car l)) (cdr l))
+          (else (cons (car l) (rember-f test? a (cdr l)))))))
