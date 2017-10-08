df3e87bc7813ac67dff5e6cb8c3a70f8b5e80f9d
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-08 11:21:15 +0530

    Implement insertR*

--- stars.scm
+++ stars.scm
@@ -5,3 +5,12 @@
         ((not (atom? (car l))) (cons (rember* a (car l))
                                      (rember* a (cdr l))))
         (else (cons (car l) (rember* a (cdr l))))))
+
+(define (insertR* new old l)
+  (cond ((null? l) '())
+        ((atom? (car l))
+         (cond
+           ((eq? old (car l)) (cons old (cons new (insertR* new old (cdr l)))))
+           (else (cons (car l) (insertR* new old (cdr l))))))
+        (else (cons (insertR* new old (car l))
+                    (insertR* new old (cdr l))))))
