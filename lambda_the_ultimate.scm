8d1e7ff1ac3573577c44e43f85dd8a752c9bc28f
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-08 16:12:43 +0530

    Implement insert-f and family

--- lambda_the_ultimate.scm
+++ lambda_the_ultimate.scm
@@ -4,3 +4,21 @@
     (cond ((null? l) '())
           ((test? a (car l)) (cdr l))
           (else (cons (car l) (rember-f test? a (cdr l)))))))
+
+(define (insert-f test? merge)
+  (lambda (new old lat)
+    (cond ((null? lat) '())
+          ((test? old (car lat)) (merge new old (cdr lat)))
+          (else (cons (car lat) ((insertL-f test? merge) new old (cdr lat)))))))
+
+(define (insertL-f test?)
+  (insert-f test? (lambda (new old lat)
+                    (cons new (cons old lat)))))
+
+(define (insertR-f test?)
+  (insert-f test? (lambda (new old lat)
+                    (cons old (cons new lat)))))
+
+(define (subst-f test?)
+  (insert-f test? (lambda (new old lat)
+                    (cons new lat))))
