36547b24a3aa173d7fe8f8712f4de1bc9600ff78
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-08 14:29:20 +0530

    Implement value

--- shadows.scm
+++ shadows.scm
@@ -3,3 +3,15 @@
   (cond ((atom? l) (number? l))
         (else (and (numbered? (car l))
                    (numbered? (caddr l))))))
+
+(define (value nexp)
+  (cond ((atom? nexp) nexp)
+        ((eq? (cadr nexp) '+)
+         (add (value (car nexp))
+              (value (caddr nexp))))
+        ((eq? (cadr nexp) '*)
+         (mul (value (car nexp))
+              (value (caddr nexp))))
+        (else
+         (pow (value (car nexp))
+              (value (caddr nexp))))))
